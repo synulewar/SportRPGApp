056b2479e308e52032438b5df2e6cba6e7bdb0fd
synulewar <synulewar@gmail.com>
2018-08-14 12:05:47 +0200

    Extend user class

--- SportRPG/app/src/main/java/com/synowkrz/sportrpg/Model/User.kt
+++ SportRPG/app/src/main/java/com/synowkrz/sportrpg/Model/User.kt
@@ -6,9 +6,23 @@ import android.arch.persistence.room.PrimaryKey
 import com.synowkrz.sportrpg.Constant.Level
 
 @Entity(tableName = "userData")
-data class User(@PrimaryKey val email: String, val name: String, var runDis: Double, var bikeDis: Double, var walkDis: Double, var score: Long, var level : Int, var experience: Long) {
+data class User(@PrimaryKey val email: String,
+                val name: String,
+                var runDis: Double,
+                var bikeDis: Double,
+                var walkDis: Double,
+                var score: Long,
+                var level : Int,
+                var experience: Long,
+                var strength: Int,
+                var agility: Int,
+                var spellPower: Int,
+                var vitality: Int,
+                var hp: Int,
+                var mana: Int) {
     @Ignore
-    constructor(email: String, name: String) : this(email, name, 0.0, 0.0, 0.0, 0, 1, 0)
+    constructor(email: String, name: String) : this(email, name, 0.0, 0.0, 0.0,
+            0, 1, 0, 1,1,1,1,1,1)
 
     fun addTrainingResulst(trainingType: TrainingType, time: Long, distance: Double, trainingScore: Long) {
         when (trainingType) {
